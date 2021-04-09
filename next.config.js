2b51a9ac8794f276a82f1cd03b1c58cb328937d2
Garth Toland <tolandgarth@gmail.com>
2021-04-09 18:09:55 +0100

    Deployment process update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  assetPrefix: prod ? '/uk-public-holidays' : '',
   basePath: prod ? '/uk-public-holidays' : '',
   future: {
     webpack5: true,
